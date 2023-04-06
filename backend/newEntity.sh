d981ff64b0776171ec9db2ffd38cd72e18fae775
radhanama <radhanama@hotmail.com>
2023-04-06 18:30:01 +0000

    simplefling

--- backend/newEntity.sh
+++ backend/newEntity.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-# Check if the name parameter is provided
 if [ -z "$1" ]; then
   echo "Please provide the name of the entity as a parameter."
   exit 1
 fi
 
-# Set the name variable
 name=$1
-
-# Set the file paths
 entity_path="Models/Entities/${name}Entity.cs"
 dto_path="Models/DTOs/${name}Dto.cs"
 mapper_path="Models/Mapper/${name}Mapper.cs"
@@ -21,59 +17,42 @@ service_interface_path="Services/Interfaces/I${name}Service.cs"
 repository_interface_path="Infrastructure/Repositories/${name}/I${name}Repository.cs"
 repository_path="Infrastructure/Repositories/${name}/${name}Repository.cs"
 
-# Create the directories if they don't exist
-mkdir -p $(dirname $entity_path)
-mkdir -p $(dirname $dto_path)
-mkdir -p $(dirname $mapper_path)
-mkdir -p $(dirname $controller_path)
-mkdir -p $(dirname $service_path)
-mkdir -p $(dirname $service_interface_path)
-mkdir -p $(dirname $repository_interface_path)
-mkdir -p $(dirname $repository_path)
+paths=("$entity_path" "$dto_path" "$mapper_path" "$controller_path" "$service_path" "$service_interface_path" "$repository_interface_path" "$repository_path")
 
-# Check if the second parameter is provided
+for path in "${paths[@]}"; do
+  mkdir -p "$(dirname $path)"
+done
+
 if [ -z "$2" ]; then
-  # Create the files
-  touch $entity_path
-  touch $dto_path
-  touch $mapper_path
-  touch $controller_path
-  touch $service_path
-  touch $service_interface_path
-  touch $repository_interface_path
-  touch $repository_path
+  for path in "${paths[@]}"; do
+    touch "$path"
+  done
 
   echo "Files created successfully."
   exit 0
 fi
 
-# Set the name_to_copy variable
 name_to_copy=$2
+source_paths=()
+dest_paths=()
 
-# Set the file paths for the copied files
-entity_path_copy="Models/Entities/${name_to_copy}Entity.cs"
-dto_path_copy="Models/DTOs/${name_to_copy}Dto.cs"
-mapper_path_copy="Models/Mapper/${name_to_copy}Mapper.cs"
-controller_path_copy="Controllers/${name_to_copy}Controller.cs"
-service_interface_path_copy="Services/Interfaces/I${name_to_copy}Service.cs"
-service_path_copy="Services/${name_to_copy}Service.cs"
-repository_interface_path_copy="Infrastructure/Repositories/${name_to_copy}/I${name_to_copy}Repository.cs"
-repository_path_copy="Infrastructure/Repositories/${name_to_copy}/${name_to_copy}Repository.cs"
-
-# Check if the copied files exist
-if [ ! -f $entity_path_copy ] || [ ! -f $dto_path_copy ] || [ ! -f $mapper_path_copy ] || [ ! -f $controller_path_copy ] || [ ! -f $service_interface_path_copy ] || [ ! -f $service_path_copy ] || [ ! -f $repository_interface_path_copy ] || [ ! -f $repository_path_copy ]; then
-  echo "Error: One or more of the copied files do not exist."
-  exit 1
-fi
-
-# Set the source and destination paths
-source_paths=("$entity_path_copy" "$dto_path_copy" "$mapper_path_copy" "$controller_path_copy" "$service_path_copy" "$service_interface_path_copy" "$repository_interface_path_copy" "$repository_path_copy")
-dest_paths=("$entity_path" "$dto_path" "$mapper_path" "$controller_path" "$service_path" "$service_interface_path" "$repository_interface_path" "$repository_path")
-
-# Loop over the source and destination paths
-for i in "${!source_paths[@]}"; do
-    source="${source_paths[$i]}"
-    dest="${dest_paths[$i]}"
-    cp "$source" "$dest"
-    sed -i "s/${name_to_copy}/${name}/g" "$dest"
+for path in "${paths[@]}"; do
+  source="${path//${name}/${name_to_copy}}"
+  source_paths+=("$source")
+  dest_paths+=("$path")
 done
+
+for i in "${!source_paths[@]}"; do
+  source="${source_paths[$i]}"
+  dest="${dest_paths[$i]}"
+  
+  if [ ! -f "$source" ]; then
+    echo "Error: $source does not exist."
+    exit 1
+  fi
+  
+  cp "$source" "$dest"
+  sed -i "s/$name_to_copy/$name/g" "$dest"
+done
+
+echo "Files copied and updated successfully."
