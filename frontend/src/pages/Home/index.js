bbc3ea19fec30a44a2eb1fc5437c878a72bdcf96
Glasscar46 <glasscar46@gmail.com>
2023-04-16 19:13:01 -0300

    migrating to pure react

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,8 +1,11 @@
 import styles from '@/styles/HomePage.module.css'
 import Head from 'next/head'
 
-export  default function Home()
+export  default function Home(props)
 {
+    console.log(props)
+    let role = "Professor"
+    let name = props.name
     return (
         <>
             <Head>
@@ -19,7 +22,7 @@ export  default function Home()
                         <span>GERDISC</span>
                     </div>
                     <div className={styles.headerOptions}>
-                    <div>Óla, Coodenador</div>
+                    <div>Óla, {name}</div>
                     </div>
                 </div>
                 <div className={styles.headerBreak}></div>
@@ -28,36 +31,48 @@ export  default function Home()
                     <pre id='pre' style={{ margin: "1rem"}}>Acesse os paneis para consulta e cadastro:</pre>
                 </div>
                 <div className={styles.dashboard}>
-                    <div className={styles.boardItem}>
+                    {(role == "Professor" || role == "Administrator") && <div className={styles.boardItem}>
                         <div id='student' className={styles.itemIcon} >
                             <img src="student.png" />
                         </div>
                         <label htmlFor='student' className={styles.iconLabel}>Students</label>
-                    </div>
-                    <div className={styles.boardItem}>
+                    </div>}
+                    {(role == "Student") && <div className={styles.boardItem}>
+                        <div id='Profile' className={styles.itemIcon} >
+                            <img src="student.png" />
+                        </div>
+                        <label htmlFor='Profile' className={styles.iconLabel}>Meu Perfil</label>
+                    </div>}
+                    {(role == "Student") && <div className={styles.boardItem}>
+                        <div id='extensions' className={styles.itemIcon} >
+                            <img className={styles.filtered} src="calender.png" />
+                        </div>
+                        <label htmlFor='extensions' className={styles.iconLabel}>Pedidos de Extensão</label>
+                    </div>}
+                    {(role == "Administrator") && <div className={styles.boardItem}>
                         <div id='professor' className={styles.itemIcon} >
                             <img src="professor.png" />
                         </div>
                         <label htmlFor='professor' className={styles.iconLabel}>Professors</label>
-                    </div>
-                    <div className={styles.boardItem}>
+                    </div>}
+                    {(role === "Administrator" || role === "Professor") && <div className={styles.boardItem}>
                         <div id='research' className={styles.itemIcon} >
                             <img src="research.png" />
                         </div>
                         <label htmlFor='research' className={styles.iconLabel}>Research</label>
-                    </div>
-                    <div className={styles.boardItem}>
+                    </div>}
+                    {(role === "Administrator" || role === "Professor") && <div className={styles.boardItem}>
                         <div id='project' className={styles.itemIcon} >
                             <img className={styles.filtered} src="lamp.png" />
                         </div>
                         <label htmlFor='project' className={styles.iconLabel}>Project</label>
-                    </div>
-                    <div className={styles.boardItem}>
+                    </div>}
+                    {(role === "Administrator") && <div className={styles.boardItem}>
                         <div id='report' className={styles.itemIcon} >
                             <img className={styles.filtered} src="report.png" />
                         </div>
                         <label htmlFor='report' className={styles.iconLabel}>Report</label>
-                    </div>
+                    </div>}
                 </div>
                 <div className={styles.footer}><img src='/cefet.png' /></div>
             </div>
--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  pageProps.name = "Emmanuel"
+  pageProps.role = "Administrator"
   return <Component {...pageProps} />
 }
