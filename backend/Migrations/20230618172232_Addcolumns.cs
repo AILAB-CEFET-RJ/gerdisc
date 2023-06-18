ffea20ac7ee1adca8b2d430aee63d87bd2e2cd0f
radhanama <radhanama@hotmail.com>
2023-06-18 17:22:58 +0000

    add migrations

---
+++ backend/Migrations/20230618172232_Addcolumns.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gerdisc.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addcolumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastNotification",
+                table: "Students",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastNotification",
+                table: "Students");
+        }
+    }
+}
