2b46c635e4e6e758bffcd2274972a8842ab1f896
radhanama <radhanama@hotmail.com>
2023-06-21 16:37:15 +0000

    doc

--- backend/Infrastructure/Providers/Interfaces/IUserContext.cs
+++ backend/Infrastructure/Providers/Interfaces/IUserContext.cs
@@ -2,9 +2,19 @@ using gerdisc.Models.Enums;
 
 namespace gerdisc.Infrastructure.Providers
 {
+    /// <summary>
+    /// Represents the user context, providing information about the current user.
+    /// </summary>
     public interface IUserContext
     {
+        /// <summary>
+        /// Gets or sets the user ID.
+        /// </summary>
         Guid? UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user role.
+        /// </summary>
         RolesEnum? Role { get; set; }
     }
 }
