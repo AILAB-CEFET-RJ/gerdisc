02729e98bb47213224915fb9380b8c5024bae140
radhanama <radhanama@hotmail.com>
2023-06-21 14:50:23 +0000

    Doc

--- backend/Infrastructure/Validations/DissertationValidator.cs
+++ backend/Infrastructure/Validations/DissertationValidator.cs
@@ -3,16 +3,27 @@ using gerdisc.Models.DTOs;
 
 namespace gerdisc.Infrastructure.Validations
 {
+    /// <summary>
+    /// Provides validation methods for orientations.
+    /// </summary>
     public class OrientationValidator
     {
         private readonly IRepository _repository;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrientationValidator"/> class.
+        /// </summary>
+        /// <param name="repository">The repository used for data access.</param>
         public OrientationValidator(IRepository repository)
         {
             _repository = repository;
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Determines whether a dissertation can be added to a project in the given orientation.
+        /// </summary>
+        /// <param name="orientationDto">The orientation DTO containing the dissertation and student IDs.</param>
+        /// <returns>A tuple with a boolean indicating whether the dissertation can be added, and a message describing the result.</returns>
         public async Task<(bool, string)> CanAddDissertationToProject(OrientationDto orientationDto)
         {
             var project = await _repository.Project.GetByIdAsync(orientationDto.Dissertation.ProjectId);
