76457c7a55aa36644be9ff706dfdbb622d4c6003
radhanama <radhanama@hotmail.com>
2023-06-22 19:50:42 +0000

    remoção de campo desnecessario

--- backend/Models/DTOs/User/UserDto.cs
+++ backend/Models/DTOs/User/UserDto.cs
@@ -11,7 +11,6 @@ namespace gerdisc.Models.DTOs
     public class UserDto
     {
         public Guid Id { get; set; }
-        public Guid? UserId { get; set; }
         public string? FirstName { get; set; }
 
         public string? LastName { get; set; }
