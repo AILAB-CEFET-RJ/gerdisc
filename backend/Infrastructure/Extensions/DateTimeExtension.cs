ced8aa27f39e95311c33321b518a01f41a9d4a1b
radhanama <radhanama@hotmail.com>
2023-06-25 23:01:40 -0300

    correão de parser

--- backend/Infrastructure/Extensions/DateTimeExtension.cs
+++ backend/Infrastructure/Extensions/DateTimeExtension.cs
@@ -1,12 +1,32 @@
-
-using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace gerdisc.Infrastructure.Extensions
 {
     public static class DateTimeExtension
     {
-        public static DateTime? Parse(this string? dateString) =>
-            dateString is null ? null : DateTime.TryParse(dateString, out var date) ? date.ToUniversalTime() : null;
+        public static DateTime? Parse(this string? dateString)
+        {
+            if (string.IsNullOrWhiteSpace(dateString))
+                return null;
+
+            string pattern = @"^\d{2}/\d{2}/\d{4}$";
+            string format = "dd/MM/yyyy";
+
+            bool isValidFormat = Regex.IsMatch(dateString, pattern);
+            DateTime date;
+
+            if (isValidFormat && DateTime.TryParseExact(dateString, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
+            {
+                return date.ToUniversalTime();
+            }
+
+            if (DateTime.TryParse(dateString, out date))
+            {
+                return date.ToUniversalTime();
+            }
+
+            return null;
+        }
     }
 }
