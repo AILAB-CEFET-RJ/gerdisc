270405de9ef880751d023f40dc508c891d225e14
radhanama <radhanama@hotmail.com>
2023-07-05 08:48:18 -0300

    changin select

--- front/src/components/select.jsx
+++ front/src/components/select.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 
-const Select = ({ options, onSelect, className="", label, name="", required=false, disabled=false, defaultValue="" }) => {
-  var selected = defaultValue
-  if(!options) options = [];
+const Select = ({
+  options,
+  onSelect,
+  className = "",
+  label,
+  name = "",
+  required = false,
+  disabled = false,
+  defaultValue = "",
+}) => {
+  const [selected, setSelected] = useState(defaultValue);
+
   const handleChange = (event) => {
-    onSelect(event.target.value);
-    selected = event.target.value
+    const value = event.target.value;
+    onSelect(value);
+    setSelected(value);
   };
 
   return (
     <div className={className}>
-    <label htmlFor={name}>{label}</label>
-    <select disabled={disabled} required={required} onChange={handleChange} name={name} id={name}>
-      {options.map((option) => (
-        <option key={option} value={option} selected={option === selected}>
-          {option}
-        </option>
-      ))}
-    </select>
+      <label htmlFor={name}>{label}</label>
+      <select
+        disabled={disabled}
+        required={required}
+        onChange={handleChange}
+        name={name}
+        id={name}
+        value={selected}
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
