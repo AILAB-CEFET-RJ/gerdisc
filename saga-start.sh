565bfd4f155686e7cd582acacb29b60b244d7243
radhanama <radhanama@hotmail.com>
2023-09-12 15:59:59 -0300

    remove sh to start the docker

--- saga-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Change to the working directory of your Saga service
-cd /home/rdasa/gerdisc/
-
-# Start the Saga service using Docker Compose
-docker-compose -f docker-compose.yaml up -d
